70027e8a27d0d27e3ff145a0dc799f0c7d75a78e
Maysam Taherian <masetaherian@gmail.com>
2019-11-15 16:26:40 -0600

    created a contact form

--- src/Views/Contact.jsx
+++ src/Views/Contact.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Footer from '../Components/Footer';
-import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
+import {Grid, Cell } from 'react-mdl';
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Contact extends Component {
     render() {
@@ -17,36 +18,74 @@ class Contact extends Component {
                    <p style={{width: '60%', margin: 'auto', paddingTop: '1em'}}>Thank you for visiting my site. I'd would enjoy discussing any opportunities your orgianization may have available.</p>
                </Cell>
                <Cell col={6}>
+                   <div className="contact-me">
                    <h2>Contact Me</h2>
                    <hr/>
-                   <div className="contact-list">
-                   <List>
-                        <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                                <i className="fa fa-phone-square" aria-hidden="true"/>
-                                (817) 832-1355
-                            </ListItemContent>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                                <i className="fab fa-linkedin-square" aria-hidden="true"/>
-                                masetaherian
-                            </ListItemContent>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
-                                <i className="fa fa-envelope" aria-hidden="true"/>
-                                MaseTaherian@gmail.com
-                            </ListItemContent>
-                        </ListItem>
-                        {/* <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                                <i className="fab fa-skype" aria-hidden="true"/>
-                                live:masetaherian
-                            </ListItemContent>
-                        </ListItem> */}
-                    </List>
-                   </div>
+                   <div className="contact-form">
+                   <Form action="https://formspree.io/xyyynjrn" method="POST">
+
+                   <FormGroup row>
+                   <Label for="name" sm={2}>Name</Label>
+                   <Col sm={10}>
+                   <Input type="text" name="name" placeholder="First & Last" />
+                   </Col>
+                   </FormGroup>
+
+                   <FormGroup row>
+                   <Label for="phone" sm={2}>phone</Label>
+                   <Col sm={10}>
+                   <Input type="tel" name="phone" placeholder="000-000-0000" />
+                   </Col>
+                   </FormGroup>  
+
+                   <FormGroup row>
+                   <Label for="email" sm={2}>Email</Label>
+                   <Col sm={10}>
+                   <Input type="email" name="email" id="exampleEmail" placeholder="Your_Name@company.com" />
+                   </Col>
+                   </FormGroup>
+
+                   <FormGroup row>
+                   <Label for="postion" sm={2}>Position</Label>
+                   <Col sm={10}>
+                   <Input type="text" name="position"  placeholder="Your Position" />
+                   </Col>
+                   </FormGroup>
+
+                   <FormGroup row>
+                   <Label for="company" sm={2}>Company</Label>
+                   <Col sm={10}>
+                   <Input type="text" name="company"  placeholder="Company Name" />
+                   </Col>
+                   </FormGroup>
+
+                    <FormGroup row>
+                    <Label for="message" sm={2}>Message</Label>
+                    <Col sm={10}>
+                    <Input type="textarea" name="message" />
+                    </Col>
+                    </FormGroup>
+
+                    {/* <FormGroup row>
+                    <Label for="exampleFile" sm={2}>File</Label>
+                    <Col sm={10}>
+                    <Input type="file" name="file" id="exampleFile" />
+                    <FormText color="muted">
+                    This is some placeholder block-level help text for the above input.
+                    It's a bit lighter and easily wraps to a new line.
+                    </FormText>
+                    </Col>
+                    </FormGroup> */}
+      
+                    <FormGroup check row>
+                    <Col sm={{ size: 10, offset: 2 }}>
+                    <Button>Submit</Button>
+                    </Col>
+                    </FormGroup>
+
+                    </Form>
+                    </div>
+                    </div>
                </Cell>
            </Grid>
            <Footer/>
