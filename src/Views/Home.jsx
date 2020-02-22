fee9963bd8e12ded458b3e4eec05ecb97c0f1e53
Maysam Taherian <masetaherian@gmail.com>
2020-02-21 21:46:50 -0600

    updated banner

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,56 +1,70 @@
-import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
-import Projects from './Projects'
-import Contact from './Contact'
-import Skills from '../Components/Skills'
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Skills from "../Components/Skills";
 
-// import Footer from '../Components/Footer';
 class Home extends Component {
-    render() {
-        return (
-        <div className="home" >
-            <Grid className="banner">
-                <Cell col={12}>
-                    <div >
-                    <h1>MASE TAHERIAN</h1>
-                    <h5>Dallas, Tx</h5>
-                    </div>
-                </Cell>
-                </Grid>
-                <div className="info-div">
-                <Grid >
-                    <Cell col={12}>
-                        <h2>Full Stack Software Engineer</h2>
-                        <hr/>
-                        <p>ReactJS | HTML/CSS | JavaScript | NodeJS | Express | Ruby on Rails | PostgreSQL | MongoDB | REST | Project Management </p>
-                        <div className="social-links">
-                            <a href="https://www.linkedin.com/in/masetaherian/" rel="noopener noreferrer" target="_blank">
-                                <i className="fab fa-linkedin-square" aria-hidden="true"/>
-                            </a>
-                            <a href="https://github.com/masetah" rel="noopener noreferrer" target="_blank">
-                            <i className="fab fa-github-square"/>
-                            </a>
-                            <a href="https://www.facebook.com/mase.taherian" rel="noopener noreferrer" target="_blank">
-                                <i className="fab fa-facebook-square" aria-hidden="true"/>
-                            </a>
-                        </div>
-                    </Cell>
-                </Grid>
-                </div>
-                <div className="home-skills">
-                    <h2>My Skills Locker</h2>
-                    <Skills />
-                </div>
-                <div className="home-projects">
-                    <h2>My Projects</h2>
-                    <Projects />
-                </div>
-                <div >
-                    <Contact />
-                </div>
+  render() {
+    return (
+      <div className="home">
+        <Grid className="banner">
+          <Cell col={12}>
+            <div>
+              <h1>MASE TAHERIAN</h1>
+              <h5>Dallas, Tx</h5>
+            </div>
+          </Cell>
+        </Grid>
+        <div className="info-div">
+          <Grid>
+            <Cell col={12}>
+              <h2>Full Stack Software Engineer</h2>
+              <hr />
+              <p>
+                ReactJS | HTML/CSS | JavaScript | NodeJS | Express | Ruby on
+                Rails | PostgreSQL | MongoDB | REST | Project Management{" "}
+              </p>
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/masetaherian/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin-square" aria-hidden="true" />
+                </a>
+                <a
+                  href="https://github.com/masetah"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-github-square" />
+                </a>
+                <a
+                  href="https://www.facebook.com/mase.taherian"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-facebook-square" aria-hidden="true" />
+                </a>
+              </div>
+            </Cell>
+          </Grid>
         </div>
-        )
-    }
+        <div className="home-skills">
+          <h2>My Skills Locker</h2>
+          <Skills />
+        </div>
+        <div className="home-projects">
+          <h2>My Projects</h2>
+          <Projects />
+        </div>
+        <div>
+          <Contact />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/Views/Projects.jsx
+++ src/Views/Projects.jsx
@@ -1,155 +1,154 @@
-import React, {Component} from 'react';
-import ProjectCard from '../Components/ProjectCard.jsx';
-// import Footer from '../Components/Footer';
-import {
-    Tabs, 
-    Tab, 
-    Grid, 
-    Cell } from 'react-mdl';
+import React, { Component } from "react";
+import ProjectCard from "../Components/ProjectCard.jsx";
+
+import { Tabs, Tab, Grid, Cell } from "react-mdl";
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state={activeTab:0};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
 
-    toggleCategories(){
-        //ALL++++++++++++++++++++++++
-        if(this.state.activeTab===0){
-            return(
-                <div className="projects-grid">
-                    <ProjectCard
-                    title="O&S Landscaping"
-                    backgroundURL="OS-image.png"
-                    description="This stunning mobile friendly site was designed for a landscaping business."
-                    githubLink="https://github.com/masetah/OS"
-                    liveDemoLink="https://www.oslawn.com"
-                    />
-                    {/* APPRAISEME APP */}
-                    <ProjectCard
-                    title="AppPraise Me"
-                    backgroundURL="apppraiseme.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/AppPraisMe"
-                    liveDemoLink="https://appraiseme.herokuapp.com"
-                    />
-                    {/* WERQOUT APP */}
-                    <ProjectCard
-                    title="Werq Out"
-                    backgroundURL="https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/WerqOut"
-                    liveDemoLink=""
-                    /> 
-                    {/* PORTFOLIO SITE */}
-                    <ProjectCard
-                    title="Portfolio Site"
-                    backgroundURL="portfolio-screenshot.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/Portfolio"
-                    liveDemoLink=""
-                    /> 
-                    {/* STARWARS SITE */}
-                    <ProjectCard
-                    title="Star Wars Galactipedia"
-                    backgroundURL="starwars.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/Star-Wars-Galactipedia"
-                    liveDemoLink="https://masetah.github.io"
-                    /> 
-                    {/* TRAVEL TOO SITE */}
-                    <ProjectCard
-                    title="Travel Too"
-                    backgroundURL="traveltoo.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/TravelToo"
-                    liveDemoLink="https://travel-too.herokuapp.com"
-                    /> 
-                </div>
-            )
-            //React+++++++++++++++++++++
-            }else if(this.state.activeTab===1){
-            return(
-                <div className="projects-grid">
-                    <ProjectCard
-                    title="O&S Landscaping"
-                    backgroundURL="OS-image.png"
-                    description="This stunning mobile friendly site was designed for a landscaping business."
-                    githubLink="https://github.com/masetah/OS"
-                    liveDemoLink="https://www.oslawn.com"
-                    />
-                    {/* APPRAISEME APP */}
-                    <ProjectCard
-                    title="AppPraise Me"
-                    backgroundURL="apppraiseme.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/AppPraisMe"
-                    liveDemoLink="https://appraiseme.herokuapp.com"
-                    />
-                    {/* WERQOUT APP */}
-                    <ProjectCard
-                    title="Werq Out"
-                    backgroundURL="https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/WerqOut"
-                    liveDemoLink=""
-                    /> 
-                    {/* PORTFOLIO SITE */}
-                    <ProjectCard
-                    title="Portfolio Site"
-                    backgroundURL="portfolio-screenshot.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/Portfolio"
-                    liveDemoLink=""
-                    /> 
-                </div>
-            )
-
-        }else if (this.state.activeTab===2){
-            return(
-                <div className="project-grid">
-                    <ProjectCard
-                    title="Star Wars Galactipedia"
-                    backgroundURL="starwars.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/Star-Wars-Galactipedia"
-                    liveDemoLink="https://masetah.github.io"
-                    /> 
-                </div>
-            )
-            //Express+++++++++++++++++++++
-        }else if(this.state.activeTab===3){
-            return(
-                <div className="project-grid">
-                    <ProjectCard 
-                    title="Travel Too"
-                    backgroundURL="traveltoo.png"
-                    description="This will be where the description for the project goes."
-                    githubLink="https://github.com/masetah/TravelToo"
-                    liveDemoLink="https://travel-too.herokuapp.com"
-                    /> 
-                </div>
-            )
-        }
-    }
-    render() {
-        return (
-        <div className="category-tabs">
-            <Tabs activeTab={this.state.activeTab} onChange={(tabId)=>this.setState({ activeTab: tabId})}ripple>
-                <Tab>All</Tab>
-               <Tab>React</Tab>
-               <Tab>JQuery</Tab>
-               <Tab>Express</Tab>
-            </Tabs>
-                <Grid >
-                    <Cell col={12}>
-                        <div className="content">{this.toggleCategories()}</div> 
-                    </Cell>
-                </Grid>
-                {/* <Footer/> */}
+  toggleCategories() {
+    //ALL++++++++++++++++++++++++
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          <ProjectCard
+            title="O&S Landscaping"
+            backgroundURL="OS-image.png"
+            description="This stunning mobile friendly site was designed for a landscaping business."
+            githubLink="https://github.com/masetah/OS"
+            liveDemoLink="https://www.oslawn.com"
+          />
+          {/* APPRAISEME APP */}
+          <ProjectCard
+            title="AppPraise Me"
+            backgroundURL="apppraiseme.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/AppPraisMe"
+            liveDemoLink="https://appraiseme.herokuapp.com"
+          />
+          {/* WERQOUT APP */}
+          <ProjectCard
+            title="Werq Out"
+            backgroundURL="https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/WerqOut"
+            liveDemoLink=""
+          />
+          {/* PORTFOLIO SITE */}
+          <ProjectCard
+            title="Portfolio Site"
+            backgroundURL="portfolio-screenshot.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/Portfolio"
+            liveDemoLink=""
+          />
+          {/* STARWARS SITE */}
+          <ProjectCard
+            title="Star Wars Galactipedia"
+            backgroundURL="starwars.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/Star-Wars-Galactipedia"
+            liveDemoLink="https://masetah.github.io"
+          />
+          {/* TRAVEL TOO SITE */}
+          <ProjectCard
+            title="Travel Too"
+            backgroundURL="traveltoo.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/TravelToo"
+            liveDemoLink="https://travel-too.herokuapp.com"
+          />
         </div>
-        )
+      );
+      //React+++++++++++++++++++++
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div className="projects-grid">
+          <ProjectCard
+            title="O&S Landscaping"
+            backgroundURL="OS-image.png"
+            description="This stunning mobile friendly site was designed for a landscaping business."
+            githubLink="https://github.com/masetah/OS"
+            liveDemoLink="https://www.oslawn.com"
+          />
+          {/* APPRAISEME APP */}
+          <ProjectCard
+            title="AppPraise Me"
+            backgroundURL="apppraiseme.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/AppPraisMe"
+            liveDemoLink="https://appraiseme.herokuapp.com"
+          />
+          {/* WERQOUT APP */}
+          <ProjectCard
+            title="Werq Out"
+            backgroundURL="https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/WerqOut"
+            liveDemoLink=""
+          />
+          {/* PORTFOLIO SITE */}
+          <ProjectCard
+            title="Portfolio Site"
+            backgroundURL="portfolio-screenshot.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/Portfolio"
+            liveDemoLink=""
+          />
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div className="project-grid">
+          <ProjectCard
+            title="Star Wars Galactipedia"
+            backgroundURL="starwars.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/Star-Wars-Galactipedia"
+            liveDemoLink="https://masetah.github.io"
+          />
+        </div>
+      );
+      //Express+++++++++++++++++++++
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div className="project-grid">
+          <ProjectCard
+            title="Travel Too"
+            backgroundURL="traveltoo.png"
+            description="This will be where the description for the project goes."
+            githubLink="https://github.com/masetah/TravelToo"
+            liveDemoLink="https://travel-too.herokuapp.com"
+          />
+        </div>
+      );
     }
+  }
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>All</Tab>
+          <Tab>React</Tab>
+          <Tab>JQuery</Tab>
+          <Tab>Express</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+        {/* <Footer/> */}
+      </div>
+    );
+  }
 }
 
 export default Projects;
